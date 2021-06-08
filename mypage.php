6992d476553356c9af1261a050f79c209eba1f7d
Ueda Ritsuki <rituki512@gmail.com>
2021-06-08 11:53:36 +0900

    test

--- mypage.php
+++ mypage.php
@@ -17,7 +17,7 @@
  <body>
    <div class="profile">
      <img class="profile_img" src="file:///C:/Users/2190492/Desktop/%E3%83%91%E3%83%BC%E3%82%BD%E3%83%8A%E3%83%AB/%E7%94%BB%E5%83%8F/%E8%89%B2%E3%80%85/%E3%83%81%E3%83%99%E3%83%83%E3%83%88%E3%82%B9%E3%83%8A%E3%82%AE%E3%83%84%E3%83%8D.jpg">
-     <div value="profile_name">yosi</div>
+     <div value="profile_name">植田律樹</div>
    </div>
    <div class="coment">頑張る</div>
    <div class="data">php</div>
