5a6a0fc88924eba55303228e94ed86dedce30797
Ueda Ritsuki <rituki512@gmail.com>
2021-06-08 11:44:17 +0900

    テスト

---
+++ mypage.php
@@ -0,0 +1,30 @@
+<!--
+ ■個人情報
+ 名前：
+ アイコン
+ ■コメント
+ コメント：
+ ■データ
+ 使う言語など： -->
+
+ <!DOCTYPE html>
+ <html lang="ja">
+ <head>
+   <meta charset="utf-8">
+   <title>プロフィール画面</title>
+   <link rel="stylesheet" href="css/mypage.css">
+ </head>
+ <body>
+   <div class="profile">
+     <img class="profile_img" src="file:///C:/Users/2190492/Desktop/%E3%83%91%E3%83%BC%E3%82%BD%E3%83%8A%E3%83%AB/%E7%94%BB%E5%83%8F/%E8%89%B2%E3%80%85/%E3%83%81%E3%83%99%E3%83%83%E3%83%88%E3%82%B9%E3%83%8A%E3%82%AE%E3%83%84%E3%83%8D.jpg">
+     <div value="profile_name">植田</div>
+   </div>
+   <div class="coment">頑張る</div>
+   <div class="data">php</div>
+
+
+ <!--ここからBootStrap用js読込-->
+ <script type="text/javascript" src="js/jquery-3.3.1.slim.min.js"></script>
+ <script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
+ </body>
+ </html>
